fb5f8f9f4434f632a0c69306cd147ff9c183f4d3
Arthur Gabriel <122728865+arthur-Gsilva@users.noreply.github.com>
2025-08-19 19:47:14 -0300

    construção do visual de pagamento
    inicio da tela de pagamento com a construção de um select para selecionar entre pegar no local ou receber em seu edereço, itens checkbox para selecionar o método de pagamento e resumo do pedido com botão para confirmar compra e posteriormente realizar o pagamento

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,7 +6,7 @@ type Props = {
 export const Button = ({ label, onClick }: Props) => {
     return(
         <button 
-            className="bg-primary text-center font-bold py-1 px-3 rounded-full"
+            className="bg-primary text-xl text-center font-bold py-3 px-6 rounded-lg"
             onClick={onClick}
         >
             {label}
